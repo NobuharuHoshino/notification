f350e5cb747f818fa44c9c4297958528ffc1d87e
NobuharuHoshino <Nobuharu.Hoshino.bp@jp.nttdata.com>
2025-11-25 15:45:04 +0900

    コミット漏れしていたクラスの追加

---
+++ src/main/java/com/toyota/tsc/notificationhub/models/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.toyota.tsc.notificationhub.models;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+    private String resultCode;
+}
